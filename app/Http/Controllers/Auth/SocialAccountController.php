f98609eeb001b302ffe52c4cde9fd87f2a86a3e0
Olaf Broms <olafbroms@gmail.com>
2018-05-28 21:08:14 -0400

    16-production - output client info

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -149,13 +149,22 @@ class SocialAccountController extends Controller
 
         //check accepted clients
         if ($this->returnEnvironment === 'client') {
-            if (config('acceptedoauthclients.' . $referrer)) {
+//            if (config('acceptedoauthclients.' . $referrer)) {
+//                $this->referer = $referrer;
+//                return true;
+//            }
+            $ref = config('acceptedoauthclients.'. $referrer)
+                ? config('acceptedoauthclients.'. $referrer)
+                : null;
+
+            if ($ref !== null) {
                 $this->referer = $referrer;
+                logger()->error(json_encode($this->referer));
                 return true;
             }
             logger()->error(json_encode(config('acceptedoauthclients.http://evenz.online')));
             logger()->error(json_encode(config('acceptedoauthclients.' . $referrer)));
-            logger()->error(json_encode($referrer));
+            logger()->error($referrer);
             logger()->error('Client and not found');
             return false;
         }
